0d602b5eabc01ce1882ef16359071ca7b3ee4eda
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-10-21 10:06:18 +0200

    separate components for main page

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+export default () => (
+  <footer>
+    <div className="container">
+      <div className="footer__content">
+        Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
+      </div>
+    </div>
+  </footer>
+)
